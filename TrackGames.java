fbe253822086e66df1767b6844cde7f3d2ab402f
acorin64 <44778696+acorin64@users.noreply.github.com>
2018-11-06 19:46:50 -0500

    Add files via upload

---
+++ TrackGames.java
@@ -0,0 +1,30 @@
+public class TrackGames {
+  public static void main(String[] args){
+
+
+    System.out.println("Hello, Welcome to Track Games!");
+    System.out.println("Please select a game by its number to play from the menu below:");
+    System.out.println("1. Rocks, Paper, Scissors");
+    System.out.println("2. Hangman");
+    System.out.println("3. War (Card Game)");
+    System.out.println("4. Quiz Game");
+    int gameID = TextIO.getlnInt();
+
+    switch (gameID) {
+      case 1:
+      //this will be a method
+        break;
+      case 2:
+      //this will be a method
+        break;
+      case 3:
+      //this will be a method
+        break;
+      case 4:
+      //this will be a method
+        break;
+    }
+
+  }
+
+}
