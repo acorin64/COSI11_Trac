b3762b7a5c416135bb58a1d3c5627b6a4aae5dce
acorin64 <44778696+acorin64@users.noreply.github.com>
2018-11-10 16:52:02 -0500

    Add files via upload

--- TrackGames.java
+++ TrackGames.java
@@ -1,36 +1,120 @@
 public class TrackGames {
   public static void main(String[] args){
 
-
     System.out.println("Hello, Welcome to Track Games!");
-    System.out.println("Please select a game by its number to play from the menu below:");
-    System.out.println("1. Rocks, Paper, Scissors");
-    System.out.println("2. Hangman");
-    System.out.println("3. War (Card Game)");
-    System.out.println("4. Quiz Game");
-    int gameID = TextIO.getlnInt();
+    
+    mainLoop: while (true) {
+      System.out.println("Please select a game by its number to play from the menu below:");
+      System.out.println("1. Rocks, Paper, Scissors");
+      System.out.println("2. Hangman");
+      System.out.println("3. War (Card Game)");
+      System.out.println("4. Quiz Game");
+      System.out.println("5. Exit Track Games");
+      int gameID = TextIO.getlnInt();
 
-    switch (gameID) {
-      case 1:
-        playRPS();  //this will be a method
-        break;
-      case 2:
-        playHangman();  //this will be a method
-        break;
-      case 3:
-        playWar();  //this will be a method
-        break;
-      case 4:
-        playQuiz();  //this will be a method
-        break;
+      switch (gameID) {
+        case 1:
+          playRPS();  //this will be a method
+          break;
+        case 2:
+          playHangman();  //this will be a method
+          break;
+        case 3:
+          playWar();  //this will be a method
+          break;
+        case 4:
+          playQuiz();  //this will be a method
+          break;
+        case 5:
+          break mainLoop;
+      }
+
     }
-
 
   }
 
   public static void playRPS() {
-    System.out.println("This game is not yet available.");
-  }
+//introduction, menu, and choosing your hand
+    System.out.println("Welcome to Rock, Paper, Scissors!");
+    System.out.println("Choose your hand (type 1, 2, or 3):");
+    System.out.println("1. Rock");
+    System.out.println("2. Paper");
+    System.out.println("3. Scissors");
+    int yourHand = TextIO.getlnInt(); //this is the player's choice of hand
+    String playerHand = "";
+
+//processing and confirming which hand you chose
+    while (true) {
+      if (yourHand == 1) {
+        playerHand = "Rock";
+        break;
+      } else if (yourHand == 2) {
+        playerHand = "Paper";
+        break;
+      } else if (yourHand == 3) {
+        playerHand = "Scissors";
+        break;
+      } else {
+        System.out.println("You didn't choose one of the options!");
+        System.out.println("Choose another option:");
+        yourHand = TextIO.getlnInt();
+      }
+    }
+    System.out.println("You choose "+playerHand);
+
+//choosing computers hand
+    int computerHand = 0;
+    String compHand = "";
+    double cHand = Math.random()*3;
+    if (cHand <= 1) {
+      computerHand = 1;
+      compHand = "Rock";
+    } else if (cHand <= 2 && cHand > 1) {
+      computerHand = 2;
+      compHand = "Paper";
+    } else if (cHand <= 3 && cHand > 2) {
+      computerHand = 3;
+      compHand = "Scissors";
+    }
+    System.out.println("The computer chooses "+compHand);
+
+//deciding and announcing who won
+    if (yourHand == 1) {  //you are rock
+      if (computerHand == 1) {  //comp is rock
+        System.out.println("It is a tie!");
+      } else if (computerHand == 2) { //comp is paper
+        System.out.println("You Lose!");
+      } else if (computerHand == 3) { //comp is scissors
+        System.out.println("You Win!");
+      }
+    } else if (yourHand == 2) { //you are paper
+      if (computerHand == 1) {  //comp is rock
+        System.out.println("You Win!");
+      } else if (computerHand == 2) { //comp is paper
+        System.out.println("It is a tie!");
+      } else if (computerHand == 3) { //comp is scissors
+        System.out.println("You Lose!");
+      }
+    } else if (yourHand == 3) { //you are scissors
+      if (computerHand == 1) {  //comp is rock
+        System.out.println("You Lose!");
+      } else if (computerHand == 2) { //comp is paper
+        System.out.println("You Win!");
+      } else if (computerHand == 3) { //comp is scissors
+        System.out.println("It is a tie!");
+      }
+    }
+
+//ask if you will play again
+    System.out.println("Would you like to play again? (Type yes or no)");
+    boolean playAgain = TextIO.getlnBoolean();
+    if (playAgain) {
+      playRPS();
+    } else {
+      return;
+    }
+
+  } //end of playRPS
 
   public static void playHangman() {
     System.out.println("This game is not yet available.");
